312010bd1e81b119ebc94b03f9e423e0cee101ac
Yusuke Akiyama <akiyama@hakoreco.com>
2020-12-30 10:46:31 +0900

    add tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,14 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
