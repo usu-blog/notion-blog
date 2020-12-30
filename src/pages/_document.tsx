771bfd406008e9b795031a40287a1f275846e9ad
Yusuke Akiyama <akiyama@hakoreco.com>
2020-12-30 22:48:32 +0900

    add: facebook messenger

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,6 +30,37 @@ class MyDocument extends Document {
           </noscript>
           <Main />
           <NextScript />
+          {/* Facebook Messenger の設置 */}
+          <div id="fb-root"></div>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml            : true,
+                  version          : 'v9.0'
+                });
+              };
+              (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/ja_JP/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+          `,
+            }}
+          ></script>
+          {/* Your Chat Plugin code */}
+          <div
+            className="fb-customerchat"
+            // @ts-ignore
+            attribution="setup_tool"
+            page_id="100406295340921"
+            theme_color="#6699cc"
+            logged_in_greeting="何かご質問はあるでしょうか。お気軽に送信していただいて構いません。"
+            logged_out_greeting="何かご質問はあるでしょうか。お気軽に送信していただいて構いません。"
+          ></div>
         </body>
       </Html>
     )
